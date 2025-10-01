9d5ae0a5e18234f32ab654e79028ad34e34ed482
fuchs-git <pasvoss@gmail.com>
2025-10-01 09:23:21 +0200

    erster push

---
+++ 001_Einführung/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Einführung</title>
+</head>
+<body>
+
+<?php
+    $zeit = time();
+?>
+
+<h1>Test PHP <?=$zeit?></h1>
+
+</body>
+</html>
