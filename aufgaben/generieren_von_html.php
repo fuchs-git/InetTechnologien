ff1470360e177df2371bbe37382f644d3f638080
fuchs <pasvoss@gmail.com>
2025-10-03 11:16:46 +0200

    Generieren von HTML-Code

---
+++ aufgaben/generieren_von_html.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <title>Generieren von HTML-Code</title>
+</head>
+<body>
+<h1>Generieren von HTML-Code</h1>
+Durch die Ausgabe von Strings, die HTML-Tags beinhalten, kann man mit PHP HTML-Code generieren. Erzeugen Sie per PHP
+eine Ausgabe vom Buchstaben "o" ähnlich im in der ersten Abbildung, wobei jede zweite Zeile die Farbe Rot aufweist.
+
+Zusatzaufgabe
+Erzeugen Sie eine Ausgabe wie im zweiten Bild (Hinweis: Man kann in HTML auch Leerzeichen mit &nbsp; erzwingen).
+
+</body>
+</html>
+<?php
