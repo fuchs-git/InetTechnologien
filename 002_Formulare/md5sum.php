29807f75ac40dd85e28febacdb7dbb6b06dda9c5
fuchs <pasvoss@gmail.com>
2025-10-27 13:56:06 +0100

    PHP Grundlagen - Formulare MD5Sum

---
+++ 002_Formulare/md5sum.php
@@ -0,0 +1,41 @@
+<?php
+
+?>
+
+
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MD5-Hashwert</title>
+</head>
+<body>
+<h1>Berechnung eines MD5-Hashwertes</h1>
+Im folgenden Beispiel wird der MD5-Hashwert eines beliebigen Ausgangstext mit Hilfe von PHP berechnet. Zur Eingabe wird
+das textarea-Element verwendet und der eingegebene Text als Wert mit dem Key ausgangstext per GET übertragen. Neben der
+Berechnung und Ausgabe des Hashwertes wird der Ausgangstext mit Hilfe der Kurzform des PHP-Tags im textarea-Element
+ausgegeben. Würde man dies nicht tun, wäre der eingegeben Text durch das Neuladen der Webseite verlorengegangen gewesen.
+<br>
+
+<?php
+$text = $_GET['ausgangstext'] ?? '';
+?>
+
+<form>
+    <textarea name="ausgangstext" cols="40" rows="10"><?= $text ?></textarea>
+    <br>
+    <button type="submit">MD5-Hashwert berechnen</button>
+</form>
+<br>
+
+<?php
+if ($text) {
+    echo "Hashwert: " . md5($text);
+}
+?>
+
+</body>
+</html>
