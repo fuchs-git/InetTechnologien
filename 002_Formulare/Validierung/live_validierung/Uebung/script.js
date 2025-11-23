450a1416e185680c59fa7f59cc530d376a83b6cc
fuchs <pasvoss@gmail.com>
2025-11-23 15:24:39 +0100

    Aufgabe QUIZ - LiveValidierung

---
+++ 002_Formulare/Validierung/live_validierung/Uebung/script.js
@@ -0,0 +1,42 @@
+'use strict'
+
+// Fragen
+let frage1 = document.getElementById("frage1");
+let frage2 = document.getElementById("frage2");
+let frage3 = document.getElementById("frage3");
+
+document.querySelectorAll('input[name="frage1"]').forEach(radio => {
+    radio.addEventListener('change', evt => {
+        let val = evt.target.value;
+        if (val === 'Iron Man') {
+            document.getElementById('v1').innerHTML = "<div class='richtig'>Richtig!</div>";
+        } else {
+            document.getElementById('v1').innerHTML = "<div class='falsch'>Falsch!</div>";
+        }
+    })
+})
+
+
+document.querySelectorAll('input[name="frage2"]').forEach(radio => {
+    radio.addEventListener('change', evt => {
+        let val = evt.target.value;
+        if (val === 'Zeitstein') {
+            document.getElementById('v2').innerHTML = "<div class='richtig'>Richtig!</div>";
+        } else {
+            document.getElementById('v2').innerHTML = "<div class='falsch'>Falsch!</div>";
+        }
+    })
+})
+
+
+document.querySelectorAll('input[name="frage3"]').forEach(radio => {
+    radio.addEventListener('change', evt => {
+        let val = evt.target.value;
+        if (val === 'Peter Quill') {
+            document.getElementById('v3').innerHTML = "<div class='richtig'>Richtig!</div>";
+        } else {
+            document.getElementById('v3').innerHTML = "<div class='falsch'>Falsch!</div>";
+        }
+    })
+})
+
