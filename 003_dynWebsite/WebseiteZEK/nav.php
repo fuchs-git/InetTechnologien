ffa529f9442926ddd85821e3d901348a438bcda9
fuchs <pasvoss@gmail.com>
2025-11-24 14:01:33 +0100

    nav update

--- 003_dynWebsite/WebseiteZEK/nav.php
+++ 003_dynWebsite/WebseiteZEK/nav.php
@@ -1,19 +1,21 @@
 <?php
 $page = $_GET["page"] ?? '';
 $seiten = ['zuckerwerk', 'eis', 'kuchen', 'geschäft', 'unternehmen'];
-
-echo '<nav>';
-echo '<ul>';
-foreach ($seiten as $seite) {
-    if ($page == $seite) {
-        $label = ucfirst($seite);
-        echo "<li><a href='?page=$seite' class='active'>$label</a></li>";
-    }
-    else {
-        $label = ucfirst($seite);
-        echo "<li><a href='?page=$seite'>$label</a></li>";
-    }
-}
-echo '</ul>';
-echo '</nav>';
 ?>
+
+<nav>
+    <ul>
+        <?php
+            foreach ($seiten as $seite) {
+                if ($page == $seite) {
+                    $label = ucfirst($seite);
+                    echo "<li><a href='?page=$seite' class='active'>$label</a></li>";
+                }
+                else {
+                    $label = ucfirst($seite);
+                    echo "<li><a href='?page=$seite'>$label</a></li>";
+                }
+            }
+        ?>
+</ul>
+</nav>
