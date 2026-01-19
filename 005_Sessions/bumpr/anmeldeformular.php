4a42824a3af1e9da685c1325dc634855b5402d77
fuchs <pasvoss@gmail.com>
2026-01-19 13:03:32 +0100

    bumpr - Fehlerkorrektur

--- 005_Sessions/bumpr/anmeldeformular.php
+++ 005_Sessions/bumpr/anmeldeformular.php
@@ -1,7 +1,7 @@
 
 <form action="function.php" method="post">
-    <input type="text" name="username" id="username" placeholder="Benutzername"><br>
-    <input type="password" name="password" id="password" placeholder="Password"><br>
+    <label for="username"></label><input type="text" name="username" id="username" placeholder="Benutzername"><br>
+    <label for="password"></label><input type="password" name="password" id="password" placeholder="Password"><br>
     <input type="submit" value="Anmelden">
     <?php
     if (isset($_SESSION['auth']) && $_SESSION['auth'] === false) {
