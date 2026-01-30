7a839292e7840748f45fe2a8d3f8946b0aa558c0
fuchs <pasvoss@gmail.com>
2026-01-30 17:15:25 +0100

    ACL Quiz - Login Vorschau

--- 005_Sessions/quiz/index.php
+++ 005_Sessions/quiz/index.php
@@ -3,24 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <title>ACL Quiz</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <?php
 require("nav.php");
 if ($_SESSION['auth']){
+    echo <<<END
+<div>
+<img src="img/Beginner.png">
+<img src="img/Medium.png">
+<img src="img/Expert.png"></div>
+END;
+
 
 } else {
     echo <<<END
 <article>
 <h1>Willkommen beim ACL-Quiz.</h1>
-Access Control Lists gehören zu den Themen, bei denen man meint, man hätte sie verstanden –
+Access Control Lists gehören zu den Themen, bei denen man meint, man hätte sie verstanden –<br>
 bis die erste Regel plötzlich alles blockiert. Oder nichts. Oder beides.<br>
-Dieses Quiz richtet sich an alle, die wissen wollen, ob ihr ACL-Wissen wirklich sitzt oder ob sie bislang nur davon ausgegangen sind, dass „deny any“ schon irgendwie regelt, was geregelt werden muss.
-<br><br>
+<br>
 <h2>Achtung Korrekturfaktor:</h2>
 Als unangemeldeter Nutzer kannst du das Quiz leider nicht durchführen.
-Melden dich an, um dein Wissen zu testen und finde heraus,
-ob du ACLs wirklich beherrschst – oder sie dich.
+Melden dich an, um dein Wissen zu testen und finde heraus,<br>
+<strong>ob du ACLs wirklich beherrschst – oder sie dich.</strong>
 </article>
 END;
 }
