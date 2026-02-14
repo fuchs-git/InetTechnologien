6e4181cebcb252f1b109766e19bfc8ad09e0893d
fuchs <pasvoss@gmail.com>
2026-02-14 16:46:20 +0100

    bumpr returns - Adressbuch

---
+++ 007_ajax/bumpr_returns/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>bumpr returns!</title>
+    <script src="script.js" defer></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php
+require("nav.php");
+?>
+
+Hello World!
+
+
+</body>
+</html>
---
+++ 007_ajax/bumpr_returns/nav.php
@@ -0,0 +1,13 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Orbitron:wght@400..900&display=swap"
+      rel="stylesheet">
+<nav>
+    <div id="navbar">
+        <div id="logo"><img src="bumpr_returns.png" alt="bumpr returns"></div>
+        <div id="menu">
+            <a href="home.html">Home</a><br>
+            <a href="about.html">About</a>
+        </div>
+    </div>
+</nav>
