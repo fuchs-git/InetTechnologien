73683f9075c4fa823fc50ad91c26bc0a8f534e10
fuchs <pasvoss@gmail.com>
2026-03-24 12:06:11 +0100

    Emoji-Editor

---
+++ aufgaben/Iconeditor/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+
+function createIconGUI(kantenlaenge){
+    let s = '';
+
+    for (let i = 0; i < kantenlaenge*kantenlaenge; i++) {
+        s += "<div></div>";
+    }
+
+    document.querySelector("#editor").innerHTML = s;
+    document.querySelector("#editor").style.width = ((15+2)*kantenlaenge) + 'px';
+
+    document.querySelectorAll('#editor div').forEach(div => {
+        div.addEventListener('click', (e) => {
+            if (div.style.backgroundColor === 'black') {
+                div.style.backgroundColor = '';
+            }
+            else div.style.backgroundColor = 'black';
+        })
+    })
+}
+
+let kantenlaenge = 15
+createIconGUI(kantenlaenge);
+
+
+document.querySelector('#reset').addEventListener('click', (e) => {
+    document.querySelectorAll('#editor div').forEach(div => {
+        div.style.backgroundColor = '';
+    })
+})
+
