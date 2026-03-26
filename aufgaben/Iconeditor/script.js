e7083b37b087d206756a275fd8fa94a95038535c
fuchs <pasvoss@gmail.com>
2026-03-26 13:43:02 +0100

    Botschaften - Uebungsaufgabe

--- aufgaben/Iconeditor/script.js
+++ aufgaben/Iconeditor/script.js
@@ -1,41 +1,33 @@
 "use strict";
 
 
-function save_icon() {
-    if (document.querySelector('#file_path').value !== "") {
+function save_icon(){
+    if (document.querySelector('#file_path').value !== ""){
         document.querySelector('#file_path').style.borderColor = 'black';
-        let temp = [[document.querySelector('#file_path').value], [document.querySelectorAll('#editor div').forEach(div => {
-                if (div.style(.backgroundColor === 'black'))
-                {
-                    temp[1].push(1);
-                }
-            else
-                {
-                    temp[1].push(0);
-                }
-            }
-        );
-    } else {
+        let temp = [[document.querySelector('#file_path').value],[]]
+    }
+    else {
         document.querySelector('#file_path').style.borderColor = 'red';
     }
 }
 
 
-function createIconGUI(kantenlaenge) {
+function createIconGUI(kantenlaenge){
     let s = '';
 
-    for (let i = 0; i < kantenlaenge * kantenlaenge; i++) {
+    for (let i = 0; i < kantenlaenge*kantenlaenge; i++) {
         s += "<div></div>";
     }
 
     document.querySelector("#editor").innerHTML = s;
-    document.querySelector("#editor").style.width = ((15 + 2) * kantenlaenge) + 'px';
+    document.querySelector("#editor").style.width = ((15+2)*kantenlaenge) + 'px';
 
     document.querySelectorAll('#editor div').forEach(div => {
         div.addEventListener('click', (e) => {
             if (div.style.backgroundColor === 'black') {
                 div.style.backgroundColor = '';
-            } else div.style.backgroundColor = 'black';
+            }
+            else div.style.backgroundColor = 'black';
         })
     })
 }
