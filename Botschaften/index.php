bc9a9bf1c5a6cd9f9c50c36b0cafdddc0de87df1
fuchs <pasvoss@gmail.com>
2026-03-26 13:42:20 +0100

    Botschaften - Uebungsaufgabe

---
+++ Botschaften/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Auslandsvertretungen</title>
+    <link rel="icon" href="favicon.ico">
+    <style>
+        /*Cookie-Banner, ist vorgegeben*/
+        #banner {
+            width: 100vw;
+            height: 100vh;
+            background-color: rgba(255, 255, 255, 0.9);
+            position: absolute;
+            left: 0;
+            top: 0;
+            z-index: 1000;
+            display: flex; /* Cookie-Banner nach Zustimmung ausblenden --> none */
+            align-items: center;
+        }
+
+        #banner div {
+            padding: 10% 20% 10% 20%;
+        }
+        /**********************/
+    </style>
+    <script src="script.js" defer></script>
+</head>
+<body>
+<!--Banner ist vorgegeben-->
+<div id="banner">
+    <div>
+        <h2>
+            Sehr geehrte Besucherin, sehr geehrter Besucher,
+        </h2>
+        <p>
+            willkommen auf unserer Webseite! Um Ihnen eine optimale Benutzererfahrung zu bieten und die Funktionalität
+            unserer Seite sicherzustellen, ist die Verwendung von Cookies und die Einrichtung einer Sitzung (Session)
+            für den Aufruf unserer Webseite zwingend erforderlich.
+        </p>
+        <p>
+            Mit dem Fortfahren auf unserer Webseite stimmen Sie der Verwendung von Cookies und der Einrichtung einer
+            Sitzung zu. Dies ermöglicht uns, unsere Dienstleistungen und Inhalte auf Ihre Bedürfnisse abzustimmen, die
+            Sicherheit der Webseite zu gewährleisten und die allgemeine Leistungsfähigkeit zu optimieren.
+        </p>
+        <p>
+            Vielen Dank für Ihr Verständnis und viel Spaß auf unserer Webseite!
+        </p>
+
+        <!-- Hier fehlt der Button -->
+
+    </div>
+</div>
+
+<!-- Hier fehlt der Inhalt -->
+
+</body>
+</html>
