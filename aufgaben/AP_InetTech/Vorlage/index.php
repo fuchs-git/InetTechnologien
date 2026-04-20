dbd0819a7008edab431106268d586da29bb10d79
fuchs <pasvoss@gmail.com>
2026-04-20 12:48:47 +0200

    Abschlussprüfung

---
+++ aufgaben/AP_InetTech/Vorlage/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8"/>
+    <link rel="shortcut icon" href="favicon.ico"/>
+    <title>Element-Sammlung</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js" defer></script>
+</head>
+<body>
+
+<main>
+    <table>
+        <caption></caption>
+        <thead>
+        <tr>
+            <th>Z</th>
+            <th>Symbol</th>
+            <th>Name</th>
+            <th>Vorhanden</th>
+            <th>Kosten</th>
+        </tr>
+        </thead>
+        <tbody id="tbody">
+
+        </tbody>
+        <tfoot>
+        <tr>
+            <th colspan="4">Gesamtkosten</th>
+            <th id="summe"></th>
+        </tr>
+        </tfoot>
+    </table>
+
+    <div id="info"></div>
+</main>
+
+</body>
+</html>
